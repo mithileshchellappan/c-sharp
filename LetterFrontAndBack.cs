8b3828ddfafde528d9b577b6cfd16e494dd534b6
mithileshchellappan <mithilesh153@gmail.com>
2023-03-24 16:17:46 +0530

    remove ok

--- LetterFrontAndBack.cs
+++ LetterFrontAndBack.cs
@@ -8,21 +8,21 @@ namespace _24_3_Proj
 {
     public class LetterFrontAndBack
     {
-            static void Main(string[] args)
-            {
-                string str =Console.ReadLine();
-            if (str.Length <= 1)
-            {
-                Console.WriteLine("Need more than 1 character");
-                return;
-            }
-                Console.WriteLine("Original string: " + str);
+        //static void Main(string[] args)
+        //{
+        //    string str = Console.ReadLine();
+        //    if (str.Length <= 1)
+        //    {
+        //        Console.WriteLine("Need more than 1 character");
+        //        return;
+        //    }
+        //    Console.WriteLine("Original string: " + str);
 
-                char lastChar = str[str.Length - 1];
-                string newStr = lastChar + str + lastChar;
+        //    char lastChar = str[str.Length - 1];
+        //    string newStr = lastChar + str + lastChar;
 
-                Console.WriteLine("New string: " + newStr);
-            }
+        //    Console.WriteLine("New string: " + newStr);
+        //}
     }
 }
 
---
+++ OKInString.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class OKInString
+{
+    public static void Main()
+    {
+       string str =  Console.ReadLine();
+        Console.WriteLine(RemoveOk(str));
+        
+    }
+
+    public static string RemoveOk(string str)
+    {
+        str = str.ToLower();
+        if (str.Contains("ok"))
+        {
+            return str.Replace("ok", "");
+        }
+        else
+        {
+            return str;
+        }
+    }
+}
