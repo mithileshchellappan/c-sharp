d8329492b6cde64d32aa788a4c6dcb81b437f3cc
mithileshchellappan <mithilesh153@gmail.com>
2023-03-28 16:16:09 +0530

    rotate matrix

--- List.cs
+++ List.cs
@@ -29,26 +29,40 @@ namespace _24_3_Proj
 
 
         }
-        public static void Main(string[] args)
-            {
-                List<Student> studentsList = new(){new Student("hello","CSE",24,101),
-                new Student("helloOne", "IT", 22, 102),
-                new Student("helloTwo", "EEE", 23, 103),
-             new Student("helloThree", "ECE", 27, 105),
-                new Student("helloFour", "EEE", 23, 107),
-             new Student("helloFive", "CSE", 22, 109),
-                new Student("helloSix", "CIVIL", 23, 1022),
-             new Student("helloSeven", "ECE", 21, 1024),
-                new Student("helloEight", "EEE", 25, 1032)};
+        //public static void Main(string[] args)
+        //    {
+        //    //   List<Student> studentsList = new(){new Student("hello","CSE",24,101),
+        //    //   new Student("helloOne", "IT", 22, 102),
+        //    //   new Student("helloTwo", "EEE", 23, 103),
+        //    //new Student("helloThree", "ECE", 27, 105),
+        //    //   new Student("helloFour", "EEE", 23, 107),
+        //    //new Student("helloFive", "CSE", 22, 109),
+        //    //   new Student("helloSix", "CIVIL", 23, 1022),
+        //    //new Student("helloSeven", "ECE", 21, 1024),
+        //    //   new Student("helloEight", "EEE", 25, 1032)};
 
-                studentsList.Sort((s1, s2) => s1.Id.CompareTo(s2.Id));
+        //    //   studentsList.Sort((s1, s2) => s1.Id.CompareTo(s2.Id));
 
-                studentsList.ForEach(student => student.Id -= 10);
+        //    //   studentsList.ForEach(student => student.Id -= 10);
 
-                foreach (Student student in studentsList.FindAll(student => student.Department.Equals("ECE") && student.Age < 25))
-                {
-                    Console.WriteLine(student);
-                }
-            }
+        //    //   foreach (Student student in studentsList.FindAll(student => student.Department.Equals("ECE") && student.Age < 25))
+        //    //   {
+        //    //       Console.WriteLine(student);
+        //    //   }
+        //    Dictionary<int, int[]> myDict = new Dictionary<int, int[]>() {
+        //        { 1,new int[]{ 2} },
+        //        { 2 ,new int[]{ 2,4}},
+        //        { 3,new int[]{ 3,2}},
+        //        { 4,new int[]{ 3,1}},
+
+        //    };
+
+        //    foreach (var keyValue in myDict.OrderBy(x => -x.Value[0]).ThenBy(x => -x.Value[1]))
+        //    {
+        //        Console.Write(keyValue.Key + " ");
+        //        foreach (int i in keyValue.Value) Console.Write(i + " ");
+        //        Console.WriteLine();
+        //    }
+        //}
     }
 }
---
+++ RotateMatrix.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24_3_Proj
+{
+    internal class RotateMatrix
+    {
+        public static void Main(string[] args)
+        {
+            int[,] matrix =
+            {
+                {1,2,3 },
+                {4,5,6 },
+                {7,8,9 }
+            };
+
+            RotateMatrixFunc(matrix, 4);
+        }
+        static void RotateMatrixFunc(int[,] matrix,int rotations,Boolean clockwise=true)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+
+            int[,] rotatedMatrix = new int[cols, rows];
+            Console.WriteLine($"Original Matrix Rotating {(clockwise?"Clockwise":"Anti ClockWise")}");
+            PrintRotates(matrix);
+            for(int i = 0; i < rotations; i++)
+            {
+                for(int row = 0; row < rows; row++)
+                {
+                    for(int col = 0;col < cols; col++)
+                    {
+                        int value = (rows - 1 - row);
+                        //Console.WriteLine($"{col} {row} origVal:{matrix[col,row]} val:{value} changeval: {matrix[col,value]}  ");
+                        //rotatedMatrix[col, rows - 1 - row] = matrix[row, col];
+                        rotatedMatrix[col, rows - 1 - row] = clockwise ? matrix[row, col] : matrix[col, row];
+                    }
+                }
+                matrix =(int[,])rotatedMatrix.Clone();
+                Console.WriteLine($"Rotation: {i}");
+                PrintRotates(rotatedMatrix);
+            }
+           
+        }
+
+        static void PrintRotates(int[,] matrix)
+
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+            
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    Console.Write(matrix[row, col] + "|");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
