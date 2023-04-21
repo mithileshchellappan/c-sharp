d13d98e9f223e3d3eac9afba07fd40b29b0e5e13
mithileshchellappan <mithilesh153@gmail.com>
2023-04-21 09:37:47 +0530

    PROJECTINIT_MOBILE RECHARGE

---
+++ PairProgramming/MobileRecharge/MobileRecharge/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MobileRecharge.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
